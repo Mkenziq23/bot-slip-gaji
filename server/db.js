fdcfe1f03ab281cf06bc4a7e8f75492e45a22e86
Muhammad Ziqqi Pramudia <92267330+Mkenziq23@users.noreply.github.com>
2026-03-11 22:18:38 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise";
 
-const db = await mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "bot_slip_gaji",
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 export default db;
