f8bd941ad647938e8e751af9f5986a63cade851b
Muhammad Ziqqi Pramudia <92267330+Mkenziq23@users.noreply.github.com>
2026-03-11 23:27:14 +0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,16 +2,16 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "mysql.railway.internal",    // host Railway
-  user: "root",                        // user MySQL
-  password: "QBsUUcvThzIJMJYBHQQirjfZTslGhOjh", // password MySQL
-  database: "railway",                 // nama database
-  port: 3306,                          // default port MySQL
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306, // default fallback
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    rejectUnauthorized: true           // SSL agar koneksi aman
+    rejectUnauthorized: true
   }
 });
 
