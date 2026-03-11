7b369aff006ed740608f561164f9935913536388
Muhammad Ziqqi Pramudia <92267330+Mkenziq23@users.noreply.github.com>
2026-03-11 22:18:04 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -155,6 +155,8 @@ wss.on("connection", async (ws, req) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("🚀 Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server jalan di port " + PORT);
 });
